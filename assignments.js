d9eaa8e18e588e85f51a7ee774773c9e82308219
patrickmziet <patrickzietkiewicz@gmail.com>
2024-11-12 17:05:20 +0000

    updated

--- assignments.js
+++ assignments.js
@@ -1,6 +1,13 @@
 const assignments = {
-  "JAMES2024": "gAAAAABnM4ejsP0FHnqoeSiA5Lf1j0cfWb2aYzpJiFtvC7kCklPujpa5Hv7JLEJJMG9jQbHmxqS6LuB567gyk_-SjWi99eWV9Q==",
-  "JOHN2024": "gAAAAABnM4ejoXqthv5KJXy6dafcH4-k9Bbi8UHG9ae2DbTlPvD1_07UjQvlSqddaEcVP_GbOzWf_78ZLIBZqicns57k3ZLzpQ==",
-  "SARAH2024": "gAAAAABnM4ej566yPrbseh_-wBEy2PWJaD1q6UQbSI2X8qfpOEwXQ_vj7G8T4ATg6Q7y22AzamFOB0rf5hamC7gFlssS4ldMDw==",
-  "EMMA2024": "gAAAAABnM4ejnZVGvK1YYvo09CKJWZS8cGrTy1hyTTp4k2HOpD5LSEt-EnytvUye-bkJIUT0F4wotY8cefqw0C5TEZWXZg0IlQ=="
+    "JAMES2024": CryptoJS.AES.encrypt("Rachel", ENCRYPTION_KEY).toString(),
+    "JOHN2024": CryptoJS.AES.encrypt("Anna", ENCRYPTION_KEY).toString(),
+    "SARAH2024": CryptoJS.AES.encrypt("Emma", ENCRYPTION_KEY).toString(),
+    "EMMA2024": CryptoJS.AES.encrypt("James", ENCRYPTION_KEY).toString(),
+    "MICHAEL2024": CryptoJS.AES.encrypt("Peter", ENCRYPTION_KEY).toString(),
+    "LISA2024": CryptoJS.AES.encrypt("Michael", ENCRYPTION_KEY).toString(),
+    "DAVID2024": CryptoJS.AES.encrypt("Sarah", ENCRYPTION_KEY).toString(),
+    "ANNA2024": CryptoJS.AES.encrypt("John", ENCRYPTION_KEY).toString(),
+    "PETER2024": CryptoJS.AES.encrypt("Tom", ENCRYPTION_KEY).toString(),
+    "RACHEL2024": CryptoJS.AES.encrypt("Lisa", ENCRYPTION_KEY).toString(),
+    "TOM2024": CryptoJS.AES.encrypt("David", ENCRYPTION_KEY).toString(),
 };
